a50e2a77f7fd50b70f42f2e2110297c187d5cbdf
Madhu Kunwar <kcmadhu081@gmail.com>
2025-03-05 12:33:30 +0545

    credentails

--- backend/index.ts
+++ backend/index.ts
@@ -32,6 +32,8 @@ app.use(cors({
     "http://localhost:5002"
   ],
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS','PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 app.use(morgan('dev'));
